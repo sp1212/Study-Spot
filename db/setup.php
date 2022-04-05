82bf178daf8db1d0159dbe769a0f996c2867693f
Joey Elsisi <joeyelsisi@gmail.com>
2022-04-05 10:27:31 -0400

    setup complete

--- db/setup.php
+++ db/setup.php
@@ -8,7 +8,8 @@ spl_autoload_register(function($classname) {
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$db = new mysqli(Config::$db["host"], Config::$db["user"], Config::$db["pass"], Config::$db["database"]);
+
+$db = new Database();
 
 $db->query("drop table if exists secretspot;");
 
@@ -28,6 +29,7 @@ $db->query(
            name text not null,
            email text not null,
            password text not null,
+           searches JSON,
            primary key (id)
           );");
 
@@ -47,41 +49,67 @@ $db->query(
            mnemonic varchar(4) not null,
            instructor text not null,
            room text not null,
-           building text not null,
-           day1 datetime,
-           day2 datetime,
-           day3 datetime,
-           start 
+           building text DEFAULT null,
+           day1 text DEFAULT null,
+           day2 text DEFAULT null,
+           day3 text DEFAULT null,
+           start text not null,
+           end text not null
           );");
 
 $resource = fopen("../dclean/uvabuildings.csv", 'r');
 while (($building = fgetcsv($resource)) != FALSE) {
     if($building[1] == 'Web-Based Course-No class mtgs')
         continue;
-    $stmt = $db->prepare("INSERT INTO building (name) VALUES (?)");
-    $stmt->bind_param("s", $building[1]);
-    $stmt->execute();
+//    $stmt = $db->prepare("INSERT INTO building (name) VALUES (?)");
+//    $stmt->bind_param("s", $building[1]);
+//    $stmt->execute();
+    $db->query("INSERT INTO building (name) VALUES (?)","s", $building[1] );
 }
 
 
-function formatDate($date){
-    $days = [];
+function formatDate($date): array
+{
+    $data = [];
+    $days = ["date1", "date2", "date3"];
+    $iter = 0;
+    $arr = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+    foreach($arr as $k){
+        $pos = strpos($date, $k);
+        //echo $pos . " " . $k;
+        if($pos !== FALSE){
+            $data[$days[$iter]] = $k;
+            $iter++;
+        }
+    }
+    foreach($days as $k){
+        if(!isset($data[$k])){
+            $data[$k] = null;
+        }
+    }
+    $str = explode(" ", $date);
+    $data["end"] = $str[3];
+    $data["start"] = $str[1];
 
-
-    return $days;
+    return $data;
 }
-
-
 
 
 //Array ( [0] => 1 [1] => AAS [2] => 1020 [3] => 100 [4] => Lecture [5] => Ashon Crawley [6] => TuTh 12:30pm - 1:45pm [7] => Wilson Hall 301 [8] => Wilson Hall )
 $resource = fopen("../data/dclean.csv", 'r');
 while (($classroom = fgetcsv($resource)) != FALSE) {
-    if($classroom[1] == 'Web-Based Course-No class mtgs')
+    if($classroom[7] == 'Web-Based Course-No class mtgs')
         continue;
-    print_r($classroom);
-    $stmt = $db->prepare("INSERT INTO classroom (number,section, mnemonic,
-                       instructor, room, building, day1, day2, day3) 
-                       VALUES (?,?,?,?,?,?,?,?,?)");
-
+//    print_r($classroom);
+    $date = formatDate($classroom[6]);
+//    echo "<pre>";
+//    print_r($date);
+//   echo "</pre>"; 
+    $db->query("INSERT INTO classroom (number,section, mnemonic,
+                       instructor, room, building, day1, day2, day3, start, end)
+                       VALUES (?,?,?,?,?,?,?,?,?,?,?)",
+        "iisssssssss",
+        $classroom[2], $classroom[3], $classroom[1], $classroom[5],
+        $classroom[7], $classroom[8], $date["date1"], $date["date2"], $date["date3"],
+        $date["start"], $date["end"]);
 }
--- test.php
+++ test.php
@@ -9,25 +9,50 @@ $date1 = "Tu 6:00pm - 6:50pm";
 $date2 = "MoWe 2:00pm - 3:15pm";
 $date3 = "MoWeFr 10:00am - 10:50am";
 
-function formatDate($date): array
-{
-    $data = [];
-    $days = ["date1", "date2", "date3"];
-    $iter = 0;
-    $arr = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
-    foreach($arr as $k){
-        $pos = strpos($date, $k);
-        echo $pos . " " . $k;
-        if($pos !== FALSE){
-            $data[$days[$iter]] = $k;
-            $iter++;
-        }
-    }
-    
-    $str = explode(" ", $date);
-    $data["end"] = $str[3];
-    $data["start"] = $str[1];
-    
-    return $data;
-}
-print_r(formatDate($date2));
+$temp = new DateTime("6:00pm");
+echo $temp->format("Ga");
+
+// https://www.php.net/manual/en/datetime.format.php
+
+//function formatTime($time): string
+//{
+//    $pm = strpos($time, "pm");
+//    $time = trim($time, "pam");
+//    echo $time;
+//    $date = new DateTime($time);
+//    if($pm !== FALSE){
+//        $temp = 0;
+//    }
+//    return "hi";
+//}
+
+
+//function formatDate($date): array
+//{
+//    $data = [];
+//    $days = ["date1", "date2", "date3"];
+//    $iter = 0;
+//    $arr = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+//    foreach($arr as $k){
+//        $pos = strpos($date, $k);
+//        //echo $pos . " " . $k;
+//        if($pos !== FALSE){
+//            $data[$days[$iter]] = $k;
+//            $iter++;
+//        }
+//    }
+//    print_r($data);
+//    foreach($days as $k){
+//        if(!isset($data[$k])){
+//            $data[$k] = null;
+//        }
+//    }
+//    $str = explode(" ", $date);
+//    $data["end"] = $str[3];
+//    $data["start"] = $str[1];
+//
+//    return $data;
+//}
+$temp = formatDate($date2);
+print_r($temp["date3"]);
+print_r($temp);
