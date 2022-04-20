63e57854820e5b8359fa509692d85a5f84f033f2
Stuart Paine <scp4exq@virginia.edu>
2022-04-20 16:02:38 -0400

    Scroll to top button (sprint 4)

--- templates/buildinglist.php
+++ templates/buildinglist.php
@@ -15,7 +15,38 @@
     <title>Buildings List</title>
 </head>
 
+<style>
+    #toTopBtn {
+    display: none;
+    position: fixed;
+    bottom: 20px;
+    right: 30px;
+    z-index: 99;
+    font-size: 18px;
+    border: none;
+    outline: none;
+    background-color: #222;
+    color: white;
+    cursor: pointer;
+    padding: 15px;
+    border-radius: 4px;
+  }
+
+  #toTopBtn:hover {
+    background-color: #555;
+  }
+</style>
+
 <body>
+
+<button onclick="topFunction()" id="toTopBtn" title="Go to top">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-bar-up"
+      viewBox="0 0 16 16">
+      <path fill-rule="evenodd"
+        d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z" />
+    </svg>
+</button>
+
 <header>
     <!--Top navigation bar-->
     <div id="top-navbar-placeholder">
@@ -41,32 +72,26 @@
         <div class="col-lg-2">
         </div>
         <div class="col">
-    <?php
-    foreach($_SESSION["buildings"] as $building){
+
+        <?php
+        foreach($_SESSION["buildings"] as $building){
+            ?>
+
+            <div class="row cr-list-item open-list-item">
+                <a href="?command=building&name=<?=$building["name"]?>" title="Rice Hall 011"></a>
+                <div class="col-4 cr-name">
+                    <?=$building["name"]?>
+                </div>
+                <div class="col-4 open">
+                    Open
+                </div>
+                <div class="col-4 until">
+                    Until 2:30pm
+                </div>
+            </div>
+            <?php
+        }
         ?>
-
-        <div class="row cr-list-item open-list-item">
-            <a href="?command=building&name=<?=$building["name"]?>" title="Rice Hall 011"></a>
-            <div class="col-4 cr-name">
-                <?=$building["name"]?>
-            </div>
-            <div class="col-4 open">
-                Open
-            </div>
-            <div class="col-4 until">
-                Until 2:30pm
-            </div>
-        </div>
-     <?php
-
-    }
-
-    ?>
-
-
-
-
-
 
             <p class="as-of">
                 <?php
@@ -84,6 +109,26 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+</script>
+<script>
+    // Reference: previous personal (scp4exq) project
+    //Get the button
+    var topButton = document.getElementById("toTopBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            topButton.style.display = "block";
+        } else {
+            topButton.style.display = "none";
+      }
+    };
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
 </script>
 </body>
 
