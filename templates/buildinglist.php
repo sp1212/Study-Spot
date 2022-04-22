0970727568b9b595d92f209cf096d8221b20bc50
Stuart Paine <scp4exq@virginia.edu>
2022-04-21 22:18:59 -0400

    alphabetical sort buttons for buildings

--- templates/buildinglist.php
+++ templates/buildinglist.php
@@ -68,18 +68,35 @@
         <h2>Select a Building</h2>
     </div>
 
+    <div class="row-12 bcr-name justify-content-center" role="group">
+            <button type="button" class="btn btn-outline-dark" id="azDownSort">A-Z <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+              fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
+                </svg>
+            </button>
+            <button type="button" class="btn btn-outline-dark" id="azUpSort">A-Z <svg
+              xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up"
+              viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z" />
+                </svg>
+            </button>
+    </div>
+
     <div class="row">
         <div class="col-lg-2">
         </div>
-        <div class="col">
+        <div class="col buildingsCol">
+
 
         <?php
         foreach($_SESSION["buildings"] as $building){
             ?>
 
-            <div class="row cr-list-item open-list-item">
+            <div class="row cr-list-item open-list-item buildingRow">
                 <a href="?command=building&name=<?=$building["name"]?>" title="Rice Hall 011"></a>
-                <div class="col-4 cr-name">
+                <div class="col-4 cr-name buildingName">
                     <?=$building["name"]?>
                 </div>
                 <div class="col-4 open">
@@ -110,12 +127,12 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
 </script>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 <script>
     // Reference: previous personal (scp4exq) project
-    //Get the button
+    // provides a scroll to top button
     var topButton = document.getElementById("toTopBtn");
 
-    // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             topButton.style.display = "block";
@@ -124,11 +141,33 @@
       }
     };
 
-    // When the user clicks on the button, scroll to the top of the document
     function topFunction() {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
     }
+</script>
+<script>
+    // Reference: previous personal (scp4exq) project
+    // sorts buildings alphabetically
+    $('#azDownSort').on('click', function () {
+      divArr = $(".buildingRow")
+      timeDiv = $(".as-of")
+      divArr.sort(function (a, b) {
+        return $(a).find(".buildingName").text() > $(b).find(".buildingName").text() ? 1 : -1;
+      })
+      $(".buildingsCol").append(divArr)
+      $(".buildingsCol").append(timeDiv)
+    });
+
+    $('#azUpSort').on('click', function () {
+      divArr = $(".buildingRow")
+      timeDiv = $(".as-of")
+      divArr.sort(function (a, b) {
+        return $(a).find(".buildingName").text() < $(b).find(".buildingName").text() ? 1 : -1;
+      })
+      $(".buildingsCol").append(divArr)
+      $(".buildingsCol").append(timeDiv)
+    });
 </script>
 </body>
 
